67e68fe58b18e8fcf6709bff09ebc85f9f60b2cb
Seth Sharp <sesharp@outlook.com>
2023-05-11 17:04:55 +1000

    wip on form values
    Changed myForm to a interface

--- check-conflicting-prs/src/check-prs.tsx
+++ check-conflicting-prs/src/check-prs.tsx
@@ -5,13 +5,20 @@ import {useState} from "react";
 
 const items = ["Item #1", "Item #2", "Item #3"];
 
-type MyForm = {
+
+interface MyForm {
     author: string,
     organisation: string,
     token: string,
 }
 
 export default function Command() {
+
+    let myForm: MyForm = {
+        author: '',
+        organisation: '',
+        token: '',
+    };
 
     const [authorError, setAuthorError] = useState<string | undefined>();
     const [organisationError, setOrganisationError] = useState<string | undefined>();
@@ -21,18 +28,20 @@ export default function Command() {
 
     function submitForm(form: MyForm) {
         // make request
-        console.log(form.author);
-        if (form.author == undefined) {
+        console.log(myForm);
+        myForm.author = form.author;
+
+        if (form.author == '') {
             setAuthorError('Name is required');
             return;
         }
 
-        if (form.organisation == undefined) {
+        if (form.organisation == '') {
             setOrganisationError('Organisation is required');
             return;
         }
 
-        if (form.token == undefined) {
+        if (form.token == '') {
             setTokenError('Token is required');
             return;
         }
@@ -49,7 +58,7 @@ export default function Command() {
         <Form
             actions={
                 <ActionPanel>
-                    <Action.SubmitForm title="Submit Answer" onSubmit={(values) => submitForm(values)} />
+                    <Action.SubmitForm title="Submit Answer" onSubmit={submitForm} />
                 </ActionPanel>
             }
         >
