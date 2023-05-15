70bbe918a7589239b0035e7f875db6669bc4864a
Seth Sharp <sesharp@outlook.com>
2023-05-15 16:57:55 +1000

    reduce title and date clutter

--- view-current-prs/src/Components/pr-list.tsx
+++ view-current-prs/src/Components/pr-list.tsx
@@ -1,21 +1,29 @@
 import { List } from "@raycast/api";
 
 export function PRList(data: [any]) {
+
+    function convertDate(date: string) {
+
+        const dateObj = new Date(date);
+
+        return `${dateObj.getDate()}/${dateObj.getMonth()+1}/${dateObj.getFullYear()}`;
+    }
+
     return (
         <List isShowingDetail>
             {data.map((item) => (
                 <List.Item
-                    title={item.node.title}
+                    title={item.node.title.substring(item.node.title.indexOf('/') + 1)}
                     key={item.node.number}
                     detail={
                         <List.Item.Detail
                             metadata={
                                 <List.Item.Detail.Metadata>
                                     <List.Item.Detail.Metadata.Label title="Basic Information" />
-                                    <List.Item.Detail.Metadata.Label title="Title" text="Revert specific changes" />
+                                    <List.Item.Detail.Metadata.Label title="Title" text={item.node.title} />
                                     <List.Item.Detail.Metadata.Label title="State" text={item.node.state} />
-                                    <List.Item.Detail.Metadata.Label title="Created" text={item.node.createdAt} />
-                                    <List.Item.Detail.Metadata.Label title="Last Updated" text={item.node.updatedAt} />
+                                    <List.Item.Detail.Metadata.Label title="Created" text={convertDate(item.node.createdAt)} />
+                                    <List.Item.Detail.Metadata.Label title="Last Updated" text={convertDate(item.node.updatedAt)} />
                                     <List.Item.Detail.Metadata.Separator />
                                     <List.Item.Detail.Metadata.Label title="Checks"/>
                                     <List.Item.Detail.Metadata.Label title="Conflicts" text={item.node.mergeable == 'CONFLICTS' ? 'Changes required' : item.node.mergeable == 'UNKOWN' ? 'other issue' : 'All g'}/>
